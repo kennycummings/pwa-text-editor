237b4484d84d9a1b544ed15a6b5436945dd81644
kennycummings <kennethccummings@gmail.com>
2024-01-30 11:40:42 -0500

    Updated webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -6,39 +6,41 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 module.exports = () => {
   return {
     mode: 'development',
-    // Entry point for files
     entry: {
       main: './src/js/index.js',
       install: './src/js/install.js',
-      cards: './src/js/cards.js'
+      cards: './src/js/cards.js',
     },
-    // Output for our bundles
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      // Should Contact and Contact Cards be changed to something else?
-      // Webpack plugin that generates our html file and injects our bundles. 
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'Contact Cards'
+        title: 'Main Application', // Customize titles based on entry points
+        chunks: ['main'], // Specify chunks to include in the HTML file
       }),
-     
-      // Injects our custom service worker
+      new HtmlWebpackPlugin({
+        template: './install.html',
+        title: 'Install Page',
+        chunks: ['install'],
+      }),
+      new HtmlWebpackPlugin({
+        template: './cards.html',
+        title: 'Contact Cards',
+        chunks: ['cards'],
+      }),
       new InjectManifest({
         swSrc: './src-sw.js',
-        swDest: 'src-sw.js',
+        swDest: 'service-worker.js', // Correct destination for service worker
       }),
-
-      // Should Contact and Contact Cards be changed to something else?
-      // Creates a manifest.json file.
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'Contact Cards',
-        short_name: 'Contact',
-        description: 'Never forget your contacts!',
+        name: 'My PWA',
+        short_name: 'PWA',
+        description: 'My Progressive Web App',
         background_color: '#225ca3',
         theme_color: '#225ca3',
         start_url: './',
@@ -52,9 +54,7 @@ module.exports = () => {
         ],
       }),
     ],
-
     module: {
-      // CSS loaders
       rules: [
         {
           test: /\.css$/i,
@@ -63,7 +63,6 @@ module.exports = () => {
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
-          // We use babel-loader in order to use ES6.
           use: {
             loader: 'babel-loader',
             options: {
